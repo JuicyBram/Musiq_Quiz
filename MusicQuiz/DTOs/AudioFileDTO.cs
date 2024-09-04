f7a1c701b353c413e3984b86865e03de0daa5fab
Bram Wallen <465862@student.fontys.nl>
2024-09-04 18:06:00 +0200

    DTO's aangepast

--- MusicQuiz/DTOs/AudioFileDTO.cs
+++ MusicQuiz/DTOs/AudioFileDTO.cs
@@ -5,9 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; } // Simplified: no circular reference
         public byte[] FullSongBase64 { get; set; }
-        public byte[] GuitarSoloBase64 { get; set; }
         public float FullSongDuration { get; set; }
-        public float GuitarSoloDuration { get; set; }
     }
 }
 
--- MusicQuiz/DTOs/CreateAudioFileDTO.cs
+++ MusicQuiz/DTOs/CreateAudioFileDTO.cs
@@ -4,9 +4,7 @@
     {
         public string Name { get; set; } // Simplified: no circular reference
         public byte[] FullSongBase64 { get; set; }
-        public byte[] GuitarSoloBase64 { get; set; }
         public float FullSongDuration { get; set; }
-        public float GuitarSoloDuration { get; set; }
     }
 }
 
